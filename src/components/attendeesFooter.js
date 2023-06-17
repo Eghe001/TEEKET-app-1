8ca0413aff35aee09663c92385407df5c3a5c759
Eghe Igbinoba <egheigbinoba7@gmail.com>
2023-06-17 12:48:56 +0100

    done

--- src/components/attendeesFooter.js
+++ src/components/attendeesFooter.js
@@ -5,7 +5,7 @@ function footer() {
     <div className=" mx-auto w-[90%] min-[548px]:w-[60%] lg:w-[30%] mb-8 text-center font-manrope">
       Hertechtrail cohort 6 Capstone project
       <p>
-        Designed by <span className="font-bold">TEAM 6</span>
+        Designed by <span className="font-bold">TEAM 6 </span>
       </p>
     </div>
   );
